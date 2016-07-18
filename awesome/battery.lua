19bde1a3ccf70eb63774ee346de4ece505c936e0
fengidri <fengidri@gmai.com>
2016-07-18 09:49:24 +0800

    Add monitor to auto display the extern screen

--- awesome/battery.lua
+++ awesome/battery.lua
@@ -10,6 +10,7 @@
 local awful = require("awful")
 local vars = require("vars")
 local wibox = require("wibox")
+local naughty = require("naughty")
 
 local function trim(s)
   return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
